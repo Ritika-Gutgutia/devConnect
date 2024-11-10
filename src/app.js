accb9327c762322cdd24284186d5b170ff530168
Ritika Gutgutia <gutgutiaritika@gmail.com>
2024-11-10 13:37:13 +0530

    Exploring HTTP methods

--- src/app.js
+++ src/app.js
@@ -1,12 +1,48 @@
 const express = require("express");
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello from the server, testingggg modee!");
+// app.use("/test", (req, res) => {
+//   res.send("Hello from the server, testingggg modee!");
+// });
+
+// app.use("/", (req, res) => {
+//   res.send("Hello from the server");
+// });
+
+// app.use("/test/2", (req, res) => {
+//   res.send("Abracadabra");
+// });
+
+// app.use((req, res) => {
+//   res.send("Hello from the server");
+// });
+
+// app.use("/user", (req, res) => {
+//   res.send("HAHAHAHAHHAAHAAAHHAHAHAHAHAHAHAHAAAAAAAAAAAA");
+// });
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Ritika", lastName: "Gutgutia" });
 });
 
-app.use((req, res) => {
-  res.send("Hello from the server");
+app.post("/user", (req, res) => {
+  console.log("Data saved successfully to the DB");
+  res.send("Data saved successfully to the DB");
+});
+
+app.put("/user", (req, res) => {
+  console.log("Data successfully updated");
+  res.send("Data updated successfully to the DB");
+});
+
+app.patch("/user", (req, res) => {
+  console.log("Data updated successfully to the DB");
+  res.send("Data updated successfully to the DB");
+});
+
+app.delete("/user", (req, res) => {
+  console.log("Data deleted successfully from the DB");
+  res.send("Data deleted successfully from the DB");
 });
 
 app.listen(3000, () => {
