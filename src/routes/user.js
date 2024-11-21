96346b5437d2cc2b981bf5357db46de9fc331a92
Ritika Gutgutia <gutgutiaritika@gmail.com>
2024-11-21 23:50:37 +0530

    Get connections API created, Feed API created

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const { connection } = require("mongoose");
+const User = require("../models/user");
 const router = express.Router();
+
+const USER_SAFE_DATA = "firstName lastName gender age about skills";
 
 router.get("/connections", userAuth, async (req, res) => {
   try {
@@ -18,11 +22,20 @@ router.get("/connections", userAuth, async (req, res) => {
           status: "accepted",
         },
       ],
-    });
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
+    const data = connections.map((row) => {
+      if (row.fromUserId._id.equals(userId)) {
+        return row.toUserId;
+      }
+
+      return row.fromUserId;
+    });
     res.json({
       message: "All Connections retrieved successfully!",
-      connections,
+      data,
     });
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
@@ -37,7 +50,7 @@ router.get("/request/received", userAuth, async (req, res) => {
     const connectionRequestReceived = await ConnectionRequest.find({
       toUserId: userId,
       status: "interested",
-    }).populate("fromUserId", "firstName lastName");
+    }).populate("fromUserId", USER_SAFE_DATA);
 
     res.json({
       message: `${loggedInUser.firstName}'s received connection requests`,
@@ -48,4 +61,57 @@ router.get("/request/received", userAuth, async (req, res) => {
   }
 });
 
+router.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const loggedInUserId = loggedInUser._id;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    const skipDocuments = (page - 1) * limit;
+
+    limit = limit > 50 ? 50 : limit;
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [
+        {
+          fromUserId: loggedInUserId,
+        },
+        {
+          toUserId: loggedInUserId,
+        },
+      ],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+
+    connectionRequest.forEach((request) => {
+      hideUsersFromFeed.add(request.fromUserId);
+      hideUsersFromFeed.add(request.toUserId);
+    });
+
+    const users = await User.find({
+      $and: [
+        {
+          _id: {
+            $nin: Array.from(hideUsersFromFeed),
+          },
+        },
+        {
+          _id: {
+            $ne: loggedInUserId,
+          },
+        },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skipDocuments)
+      .limit(limit);
+
+    res.json({
+      message: `${loggedInUser.firstName}'s feeed`,
+      users,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
 module.exports = router;
